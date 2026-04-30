286f7e424183cf481acb7f8dab2e3f16892ac114
Mokshy <mokshithgowda88@gmail.com>
2026-05-01 02:15:23 +0530

    created consumers.py for activity log

---
+++ backend/boards/consumers.py
@@ -0,0 +1,29 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ActivityConsumer(AsyncWebsocketConsumer):
+    
+    async def connect(self):
+        self.board_id = self.scope['url_route']['kwargs']['board_id']
+        self.group_name = f"board_{self.board_id}"
+        
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name,
+        )
+        
+        await self.accept()
+
+    async def disconnect(self, code):
+        
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name,
+        )
+    
+    async def activity_event(self,event):
+        await self.send(text_data=json.dumps({
+            "type": "activity",
+            "user": event["user"],
+            "action": event["action"],
+        }))
