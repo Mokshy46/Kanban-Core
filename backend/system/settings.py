69da446b517420b129bd2ea1affdf674f496facf
Mokshy <mokshithgowda88@gmail.com>
2026-06-05 02:25:11 +0530

    Fix Render env vars

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4&wzegz*+9@=e71oh(r$(872t4ddv*j*quo9n=*-$ju2vhdgw3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
